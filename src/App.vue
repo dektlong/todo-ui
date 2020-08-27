c3ae06481e11f7cd263d781d9579338b36f399c9
dektlong <dekel.tankel@gmail.com>
2020-08-26 18:58:59 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
       <p>Original Vue TodoApp project is <a href="https://vuejs.org/v2/examples/todomvc.html">here</a></p>
       <p>First modified for this tutorial by Andrew Hughes</p>
       <p>PCF modifications for this tutorial were made by Oded Shopen</p>
+      <p>Tanzu App Service modifications for this tutorial were made by Oded Shopen</p>
     </footer>
   </div>
 </template>
